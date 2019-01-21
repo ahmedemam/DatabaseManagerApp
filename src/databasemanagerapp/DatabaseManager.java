3842795eb8fb26fcdb48bf0f35c62e2fa4b7d81a
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-01-21 13:15:20 +0200

    1st commit

---
+++ src/databasemanagerapp/DatabaseManager.java
@@ -0,0 +1,58 @@
+package databasemanagerapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    private String databasePath;
+    private String databaseName;
+    // private String databaseTableName;
+    private String databaseUsername;
+    private String databaseUserPassword;
+
+    public DatabaseManager(String databasePath, String databaseName, String databaseUsername, String databaseUserPassword) {
+        this.databasePath = databasePath;
+        this.databaseName = databaseName;
+        this.databaseUsername = databaseUsername;
+        this.databaseUserPassword = databaseUserPassword;
+    }
+
+
+    public boolean databaseConnection() {
+        boolean state = true;
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            Connection connection = DriverManager.getConnection(databasePath + databaseName, databaseUsername, databaseUserPassword);
+            return true;
+
+        } catch (SQLException exception) {
+            System.out.println("databasemanagerapp.DatabaseManager.databaseConnection().error: " + exception.getMessage());
+            return false;
+        }
+    }
+
+//    public String sendExceptionError(Exception exception){
+//        return exception.getMessage();
+//    }
+    public String getDatabasePath() {
+        return databasePath;
+    }
+
+    public void setDatabasePath(String databasePath) {
+        this.databasePath = databasePath;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/databasemanagerapp/Person.java
@@ -0,0 +1,78 @@
+package databasemanagerapp;
+
+public class Person {
+
+    private int personID;
+    private String personFirstName;
+    private String personMidName;
+    private String personLastName;
+    private String personEmail;
+    private String personPhoneNumber;
+
+    public Person() {
+        personID = 0;
+        personFirstName = "";
+        personMidName = "";
+        personLastName = "";
+        personEmail = "";
+        personPhoneNumber = "";
+    }
+
+    public Person(int personID, String personFirstName, String personMidName, String personLastName, String personEmail, String personPhoneNumber) {
+        this.personID = personID;
+        this.personFirstName = personFirstName;
+        this.personMidName = personMidName;
+        this.personLastName = personLastName;
+        this.personEmail = personEmail;
+        this.personPhoneNumber = personPhoneNumber;
+    }
+
+    public int getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
+
+    public String getPersonFirstName() {
+        return personFirstName;
+    }
+
+    public void setPersonFirstName(String personFirstName) {
+        this.personFirstName = personFirstName;
+    }
+
+    public String getPersonMidName() {
+        return personMidName;
+    }
+
+    public void setPersonMidName(String personMidName) {
+        this.personMidName = personMidName;
+    }
+
+    public String getPersonLastName() {
+        return personLastName;
+    }
+
+    public void setPersonLastName(String personLastName) {
+        this.personLastName = personLastName;
+    }
+
+    public String getPersonEmail() {
+        return personEmail;
+    }
+
+    public void setPersonEmail(String personEmail) {
+        this.personEmail = personEmail;
+    }
+
+    public String getPersonPhoneNumber() {
+        return personPhoneNumber;
+    }
+
+    public void setPersonPhoneNumber(String personPhoneNumber) {
+        this.personPhoneNumber = personPhoneNumber;
+    }
+
+}
